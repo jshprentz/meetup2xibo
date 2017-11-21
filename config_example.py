5d277c3e206ce08dd60f69e3098efd0f5113d82b
Joel Shprentz <jshprentz@his.com>
2017-11-21 12:19:26 -0500

    Store Meetup API configuration

---
+++ config_example.py
@@ -0,0 +1,14 @@
+"""Example configuration should be copied to config.py and edited."""
+
+# Meetup API configuration
+MEETUP_API_CONFIG = {
+
+    # URL name for Meetup group
+    "group_url_name": "NOVA-Makers",
+
+    # API key
+    "api_key": "zvbxrpl"
+}
+
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
