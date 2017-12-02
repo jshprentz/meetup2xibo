38b800cb02afd5818932b899cd08788536cf1fdc
Joel Shprentz <jshprentz@his.com>
2017-12-02 14:35:14 -0500

    Capture example location data.

--- meetup2xibo/meetup2xibo.py
+++ meetup2xibo/meetup2xibo.py
@@ -9,6 +9,18 @@ if __name__ == "__main__":
 
     retriever = MeetupEventsRetriever(**MEETUP_API_CONFIG)
     json_events = retriever.retrieve_events_json()
-    print(json.dumps(json_events, sort_keys=True, indent=4))
-    
-    
+
+    import csv
+    with open("locations.csv", 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(("Venue Name", "Find Us"))
+
+        for event_json in json_events:
+            venue = event_json.get("venue", {"name": ""})
+            name = venue["name"]
+            find_us = event_json.get("how_to_find_us", "")
+            csv_writer.writerow((name, find_us))
+            if not name:
+                print(event_json.get("name"))
+	
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
