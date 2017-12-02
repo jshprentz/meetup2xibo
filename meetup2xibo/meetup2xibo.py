64f0bb95aeb91c19f5b443c973346e5ed228d129
Joel Shprentz <jshprentz@his.com>
2017-12-02 18:07:49 -0500

    Use event converter to list events.

--- meetup2xibo/meetup2xibo.py
+++ meetup2xibo/meetup2xibo.py
@@ -1,8 +1,10 @@
 """Retrieve events from Meetup, extract data to display on signs, and
 update the Xibo database."""
 
-from config import MEETUP_API_CONFIG 
+from config import MEETUP_API_CONFIG, LOCATION_CONFIG
 from .meetup_api import MeetupEventsRetriever
+from .location_extractor import LocationExtractor
+from .event_converter import EventConverter
 import json
 
 if __name__ == "__main__":
@@ -10,17 +12,17 @@ if __name__ == "__main__":
     retriever = MeetupEventsRetriever(**MEETUP_API_CONFIG)
     json_events = retriever.retrieve_events_json()
 
+    location_extractor = LocationExtractor.from_location_phrases(**LOCATION_CONFIG)
+    event_converter = EventConverter(location_extractor)
+
     import csv
-    with open("locations.csv", 'w', newline='') as csv_file:
+    with open("events.csv", 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(("Venue Name", "Find Us"))
+        csv_writer.writerow(("meetup_id", "name", "start_time", "end_time", "location"))
 
         for event_json in json_events:
-            venue = event_json.get("venue", {"name": ""})
-            name = venue["name"]
-            find_us = event_json.get("how_to_find_us", "")
-            csv_writer.writerow((name, find_us))
-            if not name:
-                print(event_json.get("name"))
+            event = event_converter.convert(event_json)
+            csv_writer.writerow(event)
+            
 	
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
