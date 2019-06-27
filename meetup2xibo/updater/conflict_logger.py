ec922cc81816bccd70d3fe7bf88d0e853c1109fd
Joel Shprentz <jshprentz@his.com>
2019-06-27 15:28:40 -0400

    Filter an event's places with a list of known places.

--- meetup2xibo/updater/conflict_logger.py
+++ meetup2xibo/updater/conflict_logger.py
@@ -9,6 +9,10 @@ class ConflictLogger:
 
     logger = logging.getLogger("ConflictLogger")
 
+    def __init__(self, conflict_places):
+        """Initialize with a list of places to check for conflicts."""
+        self.conflict_places = frozenset(conflict_places)
+
     def log(self, partial_event, event_location):
         """Log for later conflict analysis a partial event, its location, and
         its places."""
@@ -17,6 +21,11 @@ class ConflictLogger:
                         partial_event,
                         event_location.description,
                         event_location.places))
+
+    def filter_conflict_places(self, places):
+        """Given iterable places, return a list of places also in the conflict
+        places set."""
+        return list(self.conflict_places & set(places))
 
 
 class NullConflictLogger:
