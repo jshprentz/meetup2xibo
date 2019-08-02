045a0ec83558875753acdf04165ae494f85ef391
Joel Shprentz <jshprentz@his.com>
2019-08-01 22:07:03 -0400

    Add a careful environment with error checking.

--- meetup2xibo/updater/exceptions.py
+++ meetup2xibo/updater/exceptions.py
@@ -16,4 +16,8 @@ class JsonConversionError(Exception):
     """Raised when JSON conversion fails."""
 
 
+class MissingEnvVarError(Exception):
+    """Raisded when an expected environment variable is missing."""
+
+
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
