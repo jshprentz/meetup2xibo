172f02911b35520b8534e9e6ddb90eb0728240cc
Joel Shprentz <jshprentz@his.com>
2019-11-16 17:26:59 -0500

    Log unchecked Meetup IDs that should have been suppressed.

--- tests/updater/test_event_suppressor.py
+++ tests/updater/test_event_suppressor.py
@@ -4,6 +4,7 @@ from meetup2xibo.updater.event_suppressor import EventSuppressor
 from hypothesis import given, assume, example
 import hypothesis.strategies as st
 import pytest
+import logging
 
 meetup_ids=st.text(alphabet="abcd", min_size=3, max_size=3)
 
@@ -30,6 +31,31 @@ def test_should_suppress_in_list(meetup_ids_to_suppress):
     meetup_id = meetup_ids_to_suppress[0]
     suppressor = EventSuppressor(meetup_ids_to_suppress)
     assert suppressor.should_suppress(meetup_id)
+
+@given(meetup_ids_to_suppress=meetup_id_lists)
+def test_log_unchecked_ids_all(meetup_ids_to_suppress, caplog):
+    """Test logging all unchecked Meetup IDs."""
+    caplog.clear()
+    caplog.set_level(logging.INFO)
+    suppressor = EventSuppressor(meetup_ids_to_suppress)
+    suppressor.log_unchecked_ids()
+    assert "Suppressed Meetup ID was not checked. meetup_id=" in caplog.text
+    for meetup_id in meetup_ids_to_suppress:
+        assert meetup_id in caplog.text
+
+@given(meetup_ids_to_suppress=meetup_id_lists)
+def test_log_unchecked_ids_only(meetup_ids_to_suppress, caplog):
+    """Test logging only unchecked Meetup IDs."""
+    caplog.clear()
+    caplog.set_level(logging.INFO)
+    suppressor = EventSuppressor(meetup_ids_to_suppress)
+    for meetup_id in meetup_ids_to_suppress[:2]:
+        assert suppressor.should_suppress(meetup_id)
+    suppressor.log_unchecked_ids()
+    for meetup_id in meetup_ids_to_suppress[:2]:
+        assert meetup_id not in caplog.text
+    for meetup_id in meetup_ids_to_suppress[2:]:
+        assert meetup_id in caplog.text
 
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 autoindent
